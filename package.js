0df7d24d575f9db6216fd3512838126024843c5c
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-04 15:47:36 +0200

    Add 3.0-rc.0 to versionsFrom

--- package.js
+++ package.js
@@ -2,12 +2,12 @@
 Package.describe({
     name: 'socialize:linkable-model',
     summary: 'A package allowing linking of records in one collection with records from many other collections',
-    version: '1.0.6',
+    version: '1.0.7',
     git: 'https://github.com/copleykj/socialize-linkable-model.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom(['1.10.2', '2.3']);
+    api.versionsFrom(['1.10.2', '2.3', '3.0-rc.0']);
 
     api.use(['socialize:base-model@1.1.7']);
     api.imply(['socialize:base-model']);
