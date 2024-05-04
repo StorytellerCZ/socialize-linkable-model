4d947485f7f943e6ab37409ef1d9b2c4887f51e4
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-04 16:00:27 +0200

    Simple schema from Meteor

--- package.js
+++ package.js
@@ -2,14 +2,17 @@
 Package.describe({
     name: 'socialize:linkable-model',
     summary: 'A package allowing linking of records in one collection with records from many other collections',
-    version: '1.0.7',
+    version: '2.0.0',
     git: 'https://github.com/copleykj/socialize-linkable-model.git',
 });
 
 Package.onUse(function _(api) {
     api.versionsFrom(['1.10.2', '2.3', '3.0-rc.0']);
 
-    api.use(['socialize:base-model@1.1.7']);
+    api.use([
+        'socialize:base-model@2.0.0',
+        'aldeed:simple-schema@1.13.1'
+    ]);
     api.imply(['socialize:base-model']);
 
     api.mainModule('common/common.js');
